939a55e049d07bf2d6a465f346a2c55a3a61cc81
victorkaplunov <victorkaplunov@yandex.ru>
2018-07-15 16:12:28 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,43 @@
+import pytest
+from splinter import Browser
+
+
+@pytest.fixture(scope="module")
+def browser():
+    global browser
+    browser = Browser('chrome')
+    return browser
+
+
+@pytest.fixture(scope="module")
+def base_url():
+    """Base URL"""
+    base_url = "https://yandex.ru/"
+    return base_url
+
+
+@pytest.fixture(scope="module")
+def open_start_page(browser, base_url):
+    """Open yandex.ru"""
+    browser.visit(base_url)
+    return browser
+
+
+@pytest.fixture(scope="module")
+def open_translate_page(open_start_page):
+    """Open translate.yandex.ru/"""
+    link = browser.find_link_by_partial_href('translate').first
+    link.click()
+    return browser
+
+
+@pytest.fixture(scope="module")
+def get_translation(open_translate_page):
+    """Get text from translation field """
+    swap_button = browser.find_by_css('div.button.button_icon.button_icon_swap')
+    swap_button.click()
+    browser.find_by_id('fakeArea').fill("test")
+    translation = browser.is_text_present('испытание', wait_time=10)
+    browser.quit()
+    return translation
+
